9e457e64187b964002da8854832ac43685bd5461
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-11 22:56:47 +0200

    added common errors and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-interface Color {
-  red: number;
-  blue: string
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/forbidden-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-  blue: 'hello',
-  red: 150
-}
 
-console.log(color);
+export * from './middlewares/current-user';
+export * from './middlewares/require-auth';
+export * from './middlewares/error-handler';
+export * from './middlewares/validate-request';
 
-export default color;
