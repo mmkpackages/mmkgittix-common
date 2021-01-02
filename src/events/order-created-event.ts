ee53703a470f52aed2525d013a6a641645a1bb36
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-02 09:48:24 +0200

    include record version field in all events interfaces

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.ORDER_CREATED;
   data: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string; // should be Date ???
--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TICKET_UPDATED;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
